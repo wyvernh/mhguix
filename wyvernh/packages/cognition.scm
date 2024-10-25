bfb492912c74bd940711081d431d3e2a980f886f
WyvernH <93463398+WyvernH@users.noreply.github.com>
2024-10-24 17:46:07 -0700

    packaged cognition

--- wyvernh/packages/cognition.scm
+++ wyvernh/packages/cognition.scm
@@ -1,4 +1,4 @@
-(define-module (wyvernh packages cognition)
+(define-module (metacrank cognition)
   #:use-module (guix licenses)
   #:use-module (guix packages)
   #:use-module (guix download)
@@ -9,27 +9,32 @@
   #:use-module (gnu packages))
 
 (define-public cognition
-  (let ((commit "c20d71e4226dc8d4b8d2b88b45f96881f57143a4")
-        (version "0.1.0")
-        (url "git@github.com:metacrank/cognition-rust.git"))
-    (package
-     (name "cognition")
-     (version version)
-     (source
-      (git-checkout
-       (url url)
-       (commit commit)))
-     (build-system cargo-build-system)
-     (arguments (list #:cargo-inputs (list)))
-     (synopsis "An unopinionated programming language which offers full publicity of syntax and tokenization.")
-     (description
-      "Cognition is a fully introspective system designed so that the syntax and hierarchy structure of the
-       language is fully public; that is, a file that contains cognition code can alter the way that it is
-       being parsed, in real time. Cognition allows for this because the language is completely postfix with
-       extremely minimal syntax, and what exists of the syntax can be changed at will. Because the language
-       never reads more than it has to, and because the language allows for metaprogramming (talking about
-       symbols as if they are data, as well as code), the syntax of the language is made fluid. This allows
-       for the advanced manipulation of how the next token is tokenized, and how these tokens are arranged
-       into something like the AST without having to explicitly program a rigid syntax.")
-     (home-page "https://www.gnu.org/software/hello/")
-     (license expat))))
+  (package
+   (name "cognition")
+   (version "0.1.0")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/metacrank/cognition-rust.git")
+           (commit "e675d26861c71e52283966168ef2082b3c2228c2")))
+     (sha256
+      (base32
+       "0wgz34m315fxn949a2ciwkqn3dsc5sd85kvssigbd1yvll4qkyd6"))
+     ;;(hash (content-hash "05zxkyz9bv3j9h0xyid1rhvh3klhsmrpkf3bcs6frvlgyr2gwilj"))
+     ))
+   (build-system cargo-build-system)
+   (synopsis "An unopinionated programming language which offers full publicity of syntax and tokenization.")
+   (description
+    "Cognition is a fully introspective system designed so that the syntax and hierarchy structure of the
+     language is fully public; that is, a file that contains cognition code can alter the way that it is
+     being parsed, in real time. Cognition allows for this because the language is completely postfix with
+     extremely minimal syntax, and what exists of the syntax can be changed at will. Because the language
+     never reads more than it has to, and because the language allows for metaprogramming (talking about
+     symbols as if they are data, as well as code), the syntax of the language is made fluid. This allows
+     for the advanced manipulation of how the next token is tokenized, and how these tokens are arranged
+     into something like the AST without having to explicitly program a rigid syntax.")
+   (home-page "https://github.com/metacrank/cognition-rust")
+   (license expat)))
+
+cognition
