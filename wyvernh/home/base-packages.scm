4422448f409738d1e7c1e7695e9c2d01706996cb
WyvernH <93463398+WyvernH@users.noreply.github.com>
2024-10-25 20:31:27 -0700

    added emacs stuff

--- wyvernh/home/base-packages.scm
+++ wyvernh/home/base-packages.scm
@@ -1,6 +1,7 @@
 (define-module (wyvernh home base-packages)
   #:use-module (gnu packages admin)
   #:use-module (gnu packages emacs)
+  #:use-module (gnu packages emacs-xyz)
   #:use-module (gnu packages web)
   #:use-module (nongnu packages mozilla)
   #:use-module (nongnu packages nvidia)
@@ -9,6 +10,29 @@
 
 (define %wyvernh-base-packages
   (list emacs-next-pgtk
+	emacs-general
+	emacs-which-key
+	emacs-hydra
+
+	emacs-org
+	emacs-org-modern
+	emacs-org-pomodoro
+	emacs-org-make-toc
+	emacs-org-present
+	emacs-org-roam
+	emacs-org-appear
+	emacs-org-ql
+
+	emacs-magit
+
+	emacs-geiser
+
+	emacs-guix
+
+	emacs-vterm
+
+	emacs-elfeed
+	
 	firefox
 	cognition
 	htop
