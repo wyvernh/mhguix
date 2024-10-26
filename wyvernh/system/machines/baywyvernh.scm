7a57bfb8a71545074163121c9c9b4463fbb36758
WyvernH <93463398+WyvernH@users.noreply.github.com>
2024-10-25 17:03:36 -0700

    removed ubcsecure nmconnection

--- wyvernh/system/machines/baywyvernh.scm
+++ wyvernh/system/machines/baywyvernh.scm
@@ -48,37 +48,6 @@
 ;   (device (file-system-label "nvme-root"))
 ;   (options (plt-btrfs-mount-options "@snapshots"))))
 
-(define ubcsecure-nmconnection
-  (plain-file "ubcsecure.nmconnection"
-              "[connection]
-id=ubcsecure
-uuid=a99e522c-332e-4672-a0ad-f42816098fa0
-type=wifi
-permissions=user:matthew:;
-
-[wifi]
-mode=infrastructure
-ssid=ubcsecure
-
-[wifi-security]
-key-mgmt=wpa-eap
-psk=physics and music composition
-
-[802-1x]
-eap=peap;
-identity=mttwhtn
-password=physics and music composition
-phase2-auth=mschapv2
-
-[ipv4]
-method=auto
-
-[ipv6]
-addr-gen-mode=default
-method=auto
-
-[proxy]"))
-
 (define wyvernh-system-bawyvernh
   (operating-system
     (inherit %wyvernh-base-operating-system)
@@ -98,8 +67,9 @@ method=auto
                         (cons tty (file-append font-terminus "/share/consolefonts/ter-132n")))
                       '("tty1" "tty2" "tty3" "tty4" "tty5" "tty6"))))
      (list
-      (simple-service 'ubcsecure-nmconnection etc-service-type
-                      (list `("NetworkManager/system-connections/ubcsecure.nmconnection"
-			      ,ubcsecure-nmconnection))))))))
+      ;;(simple-service 'ubcsecure-nmconnection etc-service-type
+      ;;                (list `("NetworkManager/system-connections/ubcsecure.nmconnection"
+      ;;,ubcsecure-nmconnection)))
+      )))))
 
 wyvernh-system-bawyvernh
