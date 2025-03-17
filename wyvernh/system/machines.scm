ec69c224a5ed7ba73f19d2e112a4c9991b46f6c1
WyvernH <93463398+wyvernh@users.noreply.github.com>
2025-03-16 17:16:16 -0700

    testing

--- wyvernh/system/machines.scm
+++ wyvernh/system/machines.scm
@@ -75,9 +75,9 @@
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
             (openpgp-fingerprint
              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-         (channel
-          (name 'ollama-guix)
-          (url "https://codeberg.org/tusharhero/ollama-guix"))
+;         (channel
+;          (name 'ollama-guix)
+;          (url "https://codeberg.org/tusharhero/ollama-guix"))
          %default-channels))
 
 (define %wyvernh-groups
