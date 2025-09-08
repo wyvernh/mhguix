1e6a5cd10ea9a505f4a698f26afd4fcface4e049
WyvernH <93463398+wyvernh@users.noreply.github.com>
2025-09-08 14:26:00 -0700

    removed legacy so guix doesn't complain

--- wyvernh/legacy/home/packages/nvidia.scm
+++
@@ -1,6 +0,0 @@
-(define-module (wyvernh home packages nvidia)
-  #:use-module (nongnu packages nvidia)
-  #:export (wyvernh-nvidia-packages))
-
-(define %wyvernh-nvidia-packages
-  (list nvidia-htop))
