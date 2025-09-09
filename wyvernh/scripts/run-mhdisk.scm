dc80d72065559235b5cdb87c52cef7bef46d4930
WyvernH <93463398+wyvernh@users.noreply.github.com>
2025-09-08 17:38:56 -0700

    test

--- wyvernh/scripts/run-mhdisk.scm
+++
@@ -1,7 +0,0 @@
-;#!/usr/bin/env -S guix repl --
-;!#
-;; Usage: sudo mhdisk /path/to/system/hostname.scm
-
-(define-module (wyvernh scripts run-mhdisk))
-
-(apply (@ (wyvernh scripts mhdisk) main) (list (command-line)))
